e563fc76bc9873c8cfd1f6016dc9f0a65134d5f4
David Neal <david@reverentgeek.com>
2016-10-19 21:05:40 -0500

    all new and improved, freshly squeezed reaction gifs

--- test/example.js
+++
@@ -1,8 +0,0 @@
-/* eslint func-names: 0, no-unused-vars: 0 */
-var expect = require( "chai" ).expect;
-
-describe( "description", function() {
-  it( "description", function() {
-    // body...
-  } );
-} );
